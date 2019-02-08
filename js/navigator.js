1293925562d4ce2ce7e79593f27dddad101172ca
Vasiliy Vdovin <vasiliy@vdovin.link>
2019-02-09 00:19:15 +0100

    Added contact form.

--- js/navigator.js
+++ js/navigator.js
@@ -1,27 +1,38 @@
 $(document).ready(function() {
 
-	$(".nav-item").click(function(event) {
+	$( ".nav-item" ).click(function() {
 
-		var id = "#" + $(this).data("target");
+		var id = "#" + $(this).data( "target" );
 		
-		$(".nav-item.selected").removeClass("selected");
-		$(".box.selected").removeClass("selected");
+		$( ".nav-item.selected" ).removeClass( "selected" );
+		$( ".box.selected" ).removeClass( "selected" );
 
-		$(id).addClass("selected");
-		$(this).addClass("selected");
+		$(id).addClass( "selected" );
+		$(this).addClass( "selected" );
 
 		var offset = $(id).offset();
 
-		offset.top = offset.top + $("#canvas-container").scrollTop();
-		offset.left = offset.left - $(".navigation-container").width() + $("#canvas-container").scrollLeft();
-		
+		offset.top = offset.top + $( "#canvas-container" ).scrollTop();
+		offset.left = offset.left - $( ".navigation-container" ).width() + $( "#canvas-container" ).scrollLeft();
 
-		$("#canvas-container").animate({
+		isNowAnimated = true;
+
+		$( "#canvas-container" ).animate({
 			
 			scrollTop: offset.top - 20,
 			scrollLeft: offset.left - 20
 			
 		}, 600);
+
+	});
+
+	$( ".box" ).click(function() {
+
+		$( ".nav-item.selected" ).removeClass( "selected" );
+		$( ".box.selected" ).removeClass( "selected" );
+
+		$(this).addClass( "selected" );
+		$( '.nav-item[data-target="' + $(this).attr("id")  +'"]' ).addClass( "selected" );
 
 	});
 
